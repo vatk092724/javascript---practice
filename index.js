f5a8e2483dec3bb9dd41dc7fa1aa4e47d6f18f53
Vatsal Kanodia(SR) <vatsalkanodia2709@gmail.com>
2024-08-05 12:35:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+console.log('hello world')
+
+
+
+
+
+// Question-5
+const dict = {
+    abandon : "o leave someone, especially someone you are responsible for, with no intention of returning ",
+    abandonned : "past tense of abandon ",
+    embarassement : " shy, awkward, or guilty feelings; a feeling of being embarrassed ",
+    enthusiastic  :"eeling or showing a lot of excitement and interest about someone or something ",
+    envelope : " a flat paper container used for sending letters in  "
+
+}
+console.log(dict.embarassement)
+console.log(dict.abandon)
+
+let a = "75"
+let b = "15"
+
+// question-1
+console.log(BigInt(a)+BigInt(b))
+
+
+// question-2
+const item = {
+    name : "apple" ,
+    price : "100" ,
+    quantity : "5kg", 
+    seller : " "
+    
+
+}
+console.log("seller")
+console.log(typeof item)
+
+console.log(alert("hello world"))
+
+ 
