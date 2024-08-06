29c32f81e7c156ba3e9aade2abac3c95da80adba
Vatsal Kanodia(SR) <vatsalkanodia2709@gmail.com>
2024-08-06 20:22:26 +0530

    Create Day02.js

---
+++ Day02.js
@@ -0,0 +1,20 @@
+//comparsion operator 
+let comp1 = 6;
+let comp2 = 7;
+console.log("comp1 == comp2 is", comp1 == comp2)
+console.log("comp1 != comp2 is", comp1 != comp2)
+console.log("comp1 === comp2 is", comp1 === comp2)
+console.log("comp1 !== comp2 is", comp1 !== comp2)
+console.log("comp1 > comp2 is", comp1 > comp2) 
+console.log("comp2 > comp1 is",
+comp2 > comp1)     
+//logical operator
+let x = 5;
+let y = 6;
+console.log(x<y && x==5)
+console.log(x>y || x==5)
+console.log(!false)
+console.log(!true)
+/* this is tutorial 
+from
+code with harry */
